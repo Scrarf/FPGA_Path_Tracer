3eb428f9f20ed6d70c6f73474f25c409699e8c69
Scrarf <goshikogiorgi@gmail.com>
2025-08-05 22:57:14 +0400

    added mult pipeline support but still flawed

--- sim_cpp/sim_mult.cpp
+++ sim_cpp/sim_mult.cpp
@@ -5,10 +5,26 @@
 #include <math.h>
 #include <stdio.h>
 #include <random>
+#include <queue>
 #include "functions.h"
+
+//For long error rate testing.
+//#define SIM_STEPS 1000000000
+//For fast confirmation
+#define SIM_STEPS 1000
+
+#define PIPELINE_DELAY 5
 
 double sc_time_stamp() { return 0; }
 
+struct queue_dl {
+	int time;
+	double a;
+	double b;
+	double ans;
+};
+
+std::queue<queue_dl> expected_output;
 
 int main(int argc, char** argv) {
 
@@ -23,48 +39,88 @@ int main(int argc, char** argv) {
 
 		const std::unique_ptr<VFP21_mult> FP21_mult{new VFP21_mult{contextp.get(), "FP21_mult"}};
 
+		FP21_mult->clk = 0;
+    	
+		int error_count = 0;
 
-    int sign_a, exp_a, frac_a;
-		double val_a = random_double(-10.0, 10.0);
-		to_int(val_a, &sign_a, &exp_a, &frac_a);
-		
-		int sign_b, exp_b, frac_b;
-		double val_b = random_double(-10.0, 10.0);
-		to_int(val_b, &sign_b, &exp_b, &frac_b);
-		
-		FP21_mult->sign_a = sign_a;
-		FP21_mult->sign_b = sign_b;
-		FP21_mult->exp_a = exp_a;
-		FP21_mult->exp_b = exp_b;
-		FP21_mult->frac_a = frac_a;
-		FP21_mult->frac_b = frac_b;
+		unsigned int sign_a, exp_a, frac_a;
+		unsigned int sign_b, exp_b, frac_b;
+
+		double val_a = 0;
+		double val_b = 0;
 
 		while (!contextp->gotFinish()) {
 				contextp->timeInc(1);
 	
-				FP21_mult->clk = !FP21_mult->clk;
+			FP21_mult->clk = !FP21_mult->clk;
 
-				
+			
 	
-				
-	
+			if (FP21_mult->clk) {
+
+				val_a = random_double(-10.0, 10.0);
+				to_int(val_a, &sign_a, &exp_a, &frac_a);
+					
+				val_b = random_double(-10.0, 10.0);
+				to_int(val_b, &sign_b, &exp_b, &frac_b);
+
+				FP21_mult->sign_a = sign_a;
+				FP21_mult->sign_b = sign_b;
+				FP21_mult->exp_a = exp_a;
+				FP21_mult->exp_b = exp_b;
+				FP21_mult->frac_a = frac_a;
+				FP21_mult->frac_b = frac_b;
+
+				expected_output.push({contextp->time() + (PIPELINE_DELAY*2), val_a, val_b, val_a * val_b});
+			}
+			
+			
+ 		  	
+ 		  	if (FP21_mult->clk && contextp->time() > (PIPELINE_DELAY*2)) {
+
+ 		  		queue_dl expected_result = {0, 0, 0, 0};
+
+ 		  		if (!expected_output.empty()) {
+ 		  			expected_result = expected_output.front();
+ 		  			expected_output.pop();
+ 		  			//printf("queue_size=%d\n", expected_output.size());
+ 		  		} else {
+ 		  			printf("QUEUE_IS_EMPTY\n");
+ 		  			contextp->gotFinish(true);
+ 		  		}
+ 		  		if (expected_result.time != contextp->time()) {
+ 		  			printf("SYNCHRONIZATION_ERROR!\n");
+ 		  			contextp->gotFinish(true);
+ 		  		}
+
+ 		  		double ans_to_double = to_double(FP21_mult->sign_c_out, FP21_mult->exp_c_out, FP21_mult->frac_c_out);
+
+ 		  		if ((expected_result.ans - ans_to_double) > 1) {
+ 		  		printf("t=%d et=%d a=%f b=%f expt=%f rly=%f \n",
+ 		  			contextp->time(),
+ 		  			expected_result.time,
+ 		  			expected_result.a,
+ 		  			expected_result.b,
+ 		  			expected_result.ans,
+ 		  			ans_to_double);
+
+ 		  			error_count++;
+ 		  		}
+
+ 		  	}
+
  		  	FP21_mult->eval();
-	
-	
- 		  	VL_PRINTF("[%" PRId64 "] clk=%x val_a=%f val_b=%f exp_answer=%f calc_answer=%f\n",
- 		  	          contextp->time(),
- 		  	          FP21_mult->clk,
- 		  	          val_a,
- 		  	          val_b,
- 		  	          (val_a * val_b),
- 		  	          to_double(FP21_mult->sign_c_out, FP21_mult->exp_c_out, FP21_mult->frac_c_out));
-	
- 		  	if ((contextp->time()) > 99) {
+
+ 		  	
+
+
+ 		  	if ((contextp->time()) > SIM_STEPS) {
  		  		contextp->gotFinish(true);
  		  	}
  		      
 		}
-
+		printf("error_rate: %f%\n", (double)( (double)error_count / (double)SIM_STEPS * 100.0 * 2));
+		
 		FP21_mult->final();
 
 		//Coverage analysis (--coverage)
