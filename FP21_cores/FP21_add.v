d37882d0276cf8d0f4f668c462da935fa6cc4f5e
Scrarf <goshikogiorgi@gmail.com>
2025-08-06 19:36:30 +0400

    completly remaking add WIP

--- FP21_cores/FP21_add.v
+++ FP21_cores/FP21_add.v
@@ -1,9 +1,9 @@
+`include "FP21_cores/definitions.vh"
+
 /*
 Inputs are unpacked arrays.
 Only works for Normalized non special case numbers.
 */
-
-`include "FP21_cores/definitions.vh"
 
 module FP21_add (
 	clk,
@@ -14,80 +14,60 @@ module FP21_add (
 	exp_a,
 	exp_b
 
-	/*output*/
+	sign_c_out,
+	exp_c_out,
+	frac_c_out
 );
 
 input wire sign_a, sign_b;
-input wire [11:0] frac_a, frac_b;
-input wire [7:0] exp_a, exp_b;
+input wire signed [`exp:0] exp_a, exp_b;
+input wire [`frac:0] frac_a, frac_b;
 input wire clk;
 
-reg [8:0] exp_norm_a;
-reg [8:0] exp_norm_b;
-reg needs_swap;
-reg sign_dl_a;
-reg sign_dl_b;
-reg [11:0] frac_dl_a;
-reg [11:0] frac_dl_b;
+output reg sign_c_out;
+output reg [`exp:0] exp_c_out;
+output reg [`frac:0]frac_c_out;
 
-reg [8:0] exp_diff;
-reg [8:0] exp_norm_swap_a;
-reg [8:0] exp_norm_swap_b;
-reg sign_swap_a;
-reg sign_swap_b;
-reg [11:0] frac_swap_a;
-reg [11:0] frac_swap_b;
-reg [12:0] frac_norm_a;
-reg [12:0] frac_norm_b;
-reg [14:0] comb_frac;
 
+reg [(`frac + 1):0] frac_combined;
 
 
 always @(posedge clk) begin
 	/*===========================================================*/
-	needs_swap <= exp_a > exp_b;
 	exp_diff <= exp_a - exp_b;
+	exp_cmp <= exp_a > exp_b;
+	sign_xor <= sign_a ^ sign_b;
 
-	sign_dl_a <= sign_a;
-	sign_dl_b <= sing_b;
-
-	frac_dl_a <= frac_a;
-	frac_dl_b <= frac_b;
-
-	exp_dl_a <= exp_a;
-	exp_dl_b <= exp_b;
- 
+	frac_a_dl <= frac_a;
+	frac_b_dl <= frac_b;
 	/*===========================================================*/
-	exp_diff_dl <= exp_diff;
-
-	exp_final <= needs_swap ? exp_a : exp_b;
-
-	exp_norm_swap_a <= needs_swap ? exp_b : exp_a;
-	exp_norm_swap_b <= needs_swap ? exp_a : exp_b;
-
-	sign_swap_a <= needs_swap ? sign_dl_b : sign_dl_a;
-	sign_swap_b <= needs_swap ? sign_dl_a : sign_dl_b;
-
-	frac_swap_a <= needs_swap ? frac_dl_b : frac_dl_a;
-	frac_swap_b <= needs_swap ? frac_dl_a : frac_dl_b;
-
-	sign_xor <= sign_dl_a ^ sign_dl_b;
-	sign_and <= sign_dl_a & sign_dl_b;
-
-	/*===========================================================*/
-	frac_norm_a <= {1'b1, (frac_swap_a >> exp_diff_dl)};
-	frac_norm_b <= (1'b1, frac_swap_b);
-
-	exp_final_dl <= exp_final;
-
 	sign_xor_dl <= sign_xor;
-	sign_and_dl <= sign_and;
+
+	exp_diff_abs <= exp_diff[`exp] ? (~(exp_diff) + 1'b1);
+
+	bigger_exp <= (exp_cmp) ? exp_a_dl : exp_b_dl;
+
+	frac_to_be_shifted <= (exp_cmp) ? frac_b_dl : frac_a_dl;
+	frac_to_be_bypassed <= (exp_cmp) ? frac_a_dl : frac_b_dl;
 
 	/*===========================================================*/
-	comb_frac <= sign_xor_dl ? (frac_norm_b + frac_norm_a) : (frac_norm_b - frac_norm_a);
+	sign_xor_dl2 <= sign_xor_dl;
+	frac_to_be_bypassed_dl <= frac_to_be_bypassed;
 
-	exp_final_dl2 <= exp_final_dl;
+	frac_shifted <= frac_to_be_shifted >> exp_diff_abs;
 
+	
+
+	/*===========================================================*/
+	frac_to_be_bypassed_dl2 <= frac_to_be_bypassed_dl;
+	frac_possibly_inverted <= sign_xor_dl2 ? (~(frac_shifted) + 1'b1) : frac_shifted;
+
+
+	/*===========================================================*/
+
+	frac_combined <= frac_to_be_bypassed_dl2 + frac_possibly_inverted;
+
+	/*===========================================================*/
 
 
 end
