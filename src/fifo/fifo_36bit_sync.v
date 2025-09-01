526a0ce59cb39d371b7f1960249eb127c69a1fe5
Scrarf <goshikogiorgi@gmail.com>
2025-09-01 04:23:49 +0400

    fifo stuff

--- src/fifo/fifo_36bit_sync.v
+++ src/fifo/fifo_36bit_sync.v
@@ -4,3 +4,42 @@ WORK IN PROGRESS
 */
 
 //following https://mohammedsahil.weebly.com/uploads/1/2/8/5/12851579/fifo_report.pdf
+
+module fifo_36bit_sync (
+	input clk,
+	input rst,
+
+	input [35:0] wr_data,
+	input wr_en,
+	output full,
+
+	output [35:0] rd_data,
+	input rd_en,
+	output empty
+);
+
+reg [9:0] rd_ptr, wr_ptr;
+
+reg [35:0] memory [511:0];
+
+wire equals = (wr_ptr[8:0] == rd_ptr[8:0]);
+
+assign empty = equals & (wr_ptr[9] == rd_ptr[9]);
+assign full = equals & (wr_ptr[9] != rd_ptr[9]);
+
+assign rd_data = memory[rd_ptr[8:0]];
+
+always @(posedge clk or posedge rst) begin
+	if(rst) begin
+		rd_ptr <= 0;
+		wr_ptr <= 0;
+	end else begin
+
+		wr_ptr <= wr_ptr + wr_en;
+		rd_ptr <= rd_ptr + rd_en;
+
+		if (wr_en) memory[wr_ptr[8:0]] <= wr_data;
+	end
+end
+
+endmodule
---
+++ src/sim_cpp/sim_fifo_36bit_sync.cpp
@@ -0,0 +1,118 @@
+#include <memory>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include <math.h>
+#include <stdio.h>
+#include <random>
+#include <queue>
+#include "functions.h"
+
+#include "Vfifo_36bit_sync.h"
+
+//For long error rate testing.
+//#define SIM_STEPS 1000000000
+
+//For fast confirmation
+#define SIM_STEPS 100000
+
+#define PIPELINE_DELAY 1
+
+//no need for VCD dump since multiplication is simple enough
+
+double sc_time_stamp() { return 0; }
+
+struct str{
+	int time, val;
+}
+
+std::queue<str> expected_output;
+
+int error_count;
+double val_a;
+
+int main(int argc, char** argv) {
+
+	printf("LETS START!!! WEEEE!!!!!\n");
+
+	const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+
+	contextp->debug(0);
+	contextp->randReset(2);
+	contextp->traceEverOn(true);
+	contextp->commandArgs(argc, argv);
+
+	const std::unique_ptr<Vfifo_36bit_sync> fifo_36bit_sync{new Vfifo_36bit_sync{contextp.get(), "fifo_36bit_sync"}};
+
+	fifo_36bit_sync->clk = 0;
+    
+	
+	while (!contextp->gotFinish()) {
+			contextp->timeInc(1);
+	
+		fifo_36bit_sync->clk = !fifo_36bit_sync->clk;
+
+		
+	
+		if (fifo_36bit_sync->clk) {
+
+			val_a = val_a++;
+
+			fifo_36bit_sync->wr_data = val_a;
+
+			expected_output.push({contextp->time() + ((PIPELINE_DELAY-1)*2), val_a});
+		}
+
+		fifo_36bit_sync->eval();
+ 	  	
+ 	  	if (fifo_36bit_sync->clk && ((contextp->time() > ((PIPELINE_DELAY-1)*2)  ))) {
+
+
+ 	  		if (!expected_output.empty()) {
+ 	  			expected_result = expected_output.front();
+ 	  			expected_output.pop();
+ 	  			//printf("queue_size=%d\n", expected_output.size());
+ 	  		} else {
+ 	  			printf("QUEUE_IS_EMPTY\n");
+ 	  			contextp->gotFinish(true);
+ 	  		}
+ 	  		if (expected_result.time != contextp->time()) {
+ 	  			printf("SYNCHRONIZATION_ERROR!\n");
+ 	  			contextp->gotFinish(true);
+ 	  		}
+
+ 	  		
+
+ 	  		if (abs(expected_result.ans - ans_to_double) > 0.1) {
+ 	  		printf("t=%d val=%d",
+ 	  			contextp->time,
+ 	  			fifo_36bit_sync->rd_data);
+
+ 	  			error_count++;
+ 	  			printf("sign_c=%b exp_c=%b frac_c=%b\n",fifo_36bit_sync->sign_c_out, fifo_36bit_sync->exp_c_out, fifo_36bit_sync->frac_c_out);
+ 	  		}
+
+ 	  	}
+ 	  	
+
+ 	  	fifo_36bit_sync->eval();
+
+
+ 	  	if ((contextp->time()) > SIM_STEPS) {
+ 	  		contextp->gotFinish(true);
+ 	  	}
+ 	  	
+	}
+	printf("error_rate: %f%\n", error_count / ((double)(SIM_STEPS - PIPELINE_DELAY - 1) / 2) * 100.0);
+	
+	fifo_36bit_sync->final();
+
+	//Coverage analysis (--coverage)
+	#if VM_COVERAGE
+ 	  	//Verilated::mkdir("logs");
+ 	  	contextp->coveragep()->write("logs/coverage/coverage.dat");
+	#endif
+
+	contextp->statsPrintSummary();
+
+	return 0;
+}
