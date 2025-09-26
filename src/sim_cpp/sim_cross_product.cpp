30b7935136dc0a090780fe2e04572c4f4e203b1c
Scrarf <goshikogiorgi@gmail.com>
2025-09-26 10:40:16 +0400

    modyfing cross product for higher abstraction level (not finished)

---
+++ src/sim_cpp/sim_cross_product.cpp
@@ -0,0 +1,114 @@
+#include <memory>
+#include <queue>
+#include <cmath>
+#include <cstdio>
+#include "functions.h"
+#include "Vcross_product.h"
+#include "verilator_skeleton/verilator_if.h"
+
+#include <verilated_vcd_c.h>
+
+//#define ENABLE_DUMP
+
+#define CLOCK_HIGH (dut->clk)
+
+int SIM_STEPS = 10000;
+int PIPELINE_DELAY = 17;
+
+struct expected_result {
+    int time;
+    double in_arr[6];
+    double out_arr[3];
+};
+
+
+static double rand_double[6];
+
+static Vcross_product* dut;
+static std::queue<expected_result> expected_output;
+
+#ifdef ENABLE_DUMP
+VerilatedVcdC* tfp = new VerilatedVcdC;
+#endif
+
+void cross_product(double* out_arr, double in_arr) {
+	
+}
+
+
+void tb_init(VerilatedContext* contextp) {
+    dut = new Vcross_product(contextp, "cross_product");
+    dut->clk = 1;
+
+    #ifdef ENABLE_DUMP
+    dut->trace(tfp, 99);
+    tfp->open("logs/vcd/cross_product.vcd");
+    #endif
+}
+
+void tb_eval(VerilatedContext* contextp, int* error_count, int* itteration_count) {
+    dut->clk = !dut->clk;
+
+    if (CLOCK_HIGH) {
+    	for (int i = 0; i < 6; i++) {
+        	val[i] = random_double(-10, 10);
+    	}
+
+  
+		uint32_t p_float3_a[3], p_float3_b[3];
+		
+		double3_to_packed_float3(p_float3_a, rand_double[0], rand_double[1], rand_double[2]);
+		double3_to_packed_float3(p_float3_b, rand_double[3], rand_double[4], rand_double[5]);
+        
+        dut->a = p_float3_a;
+        dut->b = p_float3_b;
+
+        expected_output.push({(int)contextp->time() + (PIPELINE_DELAY * 2), val_a, val_b, val_a * val_b});
+    }
+
+    if (CLOCK_HIGH && contextp->time() > (PIPELINE_DELAY * 2)) {
+        if (expected_output.empty()) {printf("QUEUE_IS_EMPTY!\n"); };
+
+        auto expected = expected_output.front();
+        expected_output.pop();
+
+        if (expected.time != contextp->time()) {
+            printf("SYNC_ERROR!\n expected:%d, contextp:%d.\n", expected.time, contextp->time());
+            contextp->gotFinish(true);
+            return;
+        }
+
+        double got = packed_array_to_double(dut->c);
+
+        
+
+        if (fabs(expected.c - got) > 0.1) {
+            printf("Mismatch at t=%d: %.4f + %.4f = %.4f (got %.4f)\n",
+                   contextp->time(), expected.a, expected.b, expected.c, got);
+            //printf("Raw packed array: %.23b\n", dut->c);
+            (*error_count)++;
+        }
+        (*itteration_count)++;
+
+    
+    }
+
+    #ifdef ENABLE_DUMP
+        tfp->dump(contextp->time());
+    #endif
+
+    dut->eval();
+}
+
+void tb_end() {
+    #ifdef ENABLE_DUMP
+    tfp->close();
+    printf("Dump successful\n");
+    #endif
+
+    dut->final();
+    delete dut;
+    return;
+}
+
+
