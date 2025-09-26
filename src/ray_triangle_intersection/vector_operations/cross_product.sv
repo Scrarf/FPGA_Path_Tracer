a50f8caaff50995e3da3873f2f2c339aa7fe9067
Scrarf <goshikogiorgi@gmail.com>
2025-09-26 10:42:16 +0400

    rewrite

--- src/ray_triangle_intersection/vector_operations/cross_product.sv
+++ src/ray_triangle_intersection/vector_operations/cross_product.sv
@@ -1,41 +1,28 @@
-`include "src/FP21_cores/definitions.vh"
+`include "src/arithmetic_cores/definitions.vh"
 
 
 module cross_product (
-	input sign_in [11:0],
-	input [`exp:0] exp_in [11:0],
-	input [`frac:0] frac_in [11:0],
-
-	output sign_out [2:0],
-	output [`exp:0] exp_out [2:0],
-	output [`frac:0] frac_out [2:0],
-
-	input wire clk
-
+	input wire clk,
+	input p_float3 a,
+	input p_float3 b,
+	output p_float3 c
 );
 
-wire net_sign [5:0];
-wire [`exp:0] net_exp [5:0];
-wire [`frac:0] net_frac [5:0];
 
-genvar i;
-generate
-	for (i = 0; i < 6; i++) begin
-		FP21_mult mult0(.clk(clk), .sign_a(sign_in[(2*i)]), .sign_b(sign_in[(2*i)+1]), .sign_c_out(net_sign[i]),
-								   .exp_a(exp_in[(2*i)]), .exp_b(exp_in[(2*i)+1]), .exp_c_out(net_exp[i]),
-								   .frac_a(frac_in[(2*i)]), .frac_b(frac_in[(2*i)+1]), .frac_c_out(net_frac[i]));
+mult mult_0(clk, a.y, b.z, net[0]);
+mult mult_1(clk, a.z, b.y, net[1]);
 
+mult mult_2(clk, a.z, b.x, net[2]);
+mult mult_3(clk, a.x, b.z, net[3]);
 
-	end
-endgenerate
+mult mult_4(clk, a.x, b.y, net[4]);
+mult mult_5(clk, a.y, b.x, net[5]);
 
-generate
-	for (i = 0; i < 3; i++) begin
-		FP21_add add0(.clk(clk), .sign_a(net_sign[(2*i)]), .sign_b(net_sign[(2*i)+1]), .sign_c_out(sign_out[i]),
-								 .exp_a(net_exp[(2*i)]), .exp_b(net_exp[(2*i)+1]), .exp_c_out(exp_out[i]),
-								 .frac_a(net_frac[(2*i)]), .frac_b(net_frac[(2*i)+1]), .frac_c_out(frac_out[i]));
-	end
-endgenerate
+p_float [5:0] net;
+
+add add_0(clk, net[0], {~net[1].sign, net[1].exp, net[1].frac}, c.x);
+add add_1(clk, net[2], {~net[3].sign, net[3].exp, net[3].frac}, c.y);
+add add_2(clk, net[4], {~net[5].sign, net[5].exp, net[5].frac}, c.z);
 
 
 endmodule
